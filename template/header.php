653ee8720c0623c4e1bf047a68eb58d0c5f9d63b
VinhNHT <vinh.the@nxcomm.com>
2017-10-27 11:34:08 +0700

    up code

---
+++ template/header.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHỤ KIỆN GIÁ RẺ</title>
+        
+        <link rel="stylesheet" type="text/css" href="resources/css/main.css">
+        <script src="resources/js/jquery.min.js" ></script>
+        <script src="resources/js/jquery-ui.min.js" ></script>
+        <script src="resources/js/common.js"></script>
+    </head>
+        
+    <div class="menu">
+        <div class="logomenu">
+            <img class="logohatec" src="resources/img/logopk.png"/>
+        </div>
+        <div class="tencongtymenu">
+            <div class="tencty">PHỤ KIỆN GIÁ RẺ VT</div>
+            <div class="diachi1">Tel: 0166 381 0003</div>
+            <div class="diachi2">Địa chỉ: 15A Nguyễn Trung Trực, F.5, Q. Bình Thạnh, Tp.HCM</div>
+        </div>
+        <div class="fanpagehatec">
+            <img class="fanpagehatecHinh" src="resources/img/fanpagepk.png">
+        </div>
+        <div class="hotlinemenu">
+            <img class="hotline" src="resources/img/dienthoaiext0.png">
+        </div>
+        <div class="vohangmenu">
+            <img class="vohangmenu_hinh imggiohang" src="resources/img/giohang.png">
+            <div class="vohangmenu_soluong soluonghangdat">0</div>
+        </div>
+    </div>
+
+<!--         <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.5";
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
+        <div style="position:fixed; z-index:9999999; right:10px; bottom:10px;" class="fb-page" data-tabs="messages"
+        data-href="https://www.facebook.com/shopruouvangdalat/" data-width="250" data-height="300" 
+        data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"></div>-->
+</html>
