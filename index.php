e4e707a530c8f12929df49c9182183b36dc0ce0d
VinhNHT <vinh.the@nxcomm.com>
2017-11-23 17:59:56 +0700

    up code

--- index.php
+++ index.php
@@ -9,9 +9,130 @@ and open the template in the editor.
         <div class="outerwapper" style="background: url(http://s.baomoi.xdn.vn/web/styles/img/hoa-dao.png?v0.10) no-repeat;     background-position: calc(50% - 574px) 0; height: 1000px">
             <?php include 'template/header.php';?>
             <div class="content">
+                <div class="widget-title">
+                    <div style="text-align: center;">
+                    <span style="color: blue;font-size: 25px;"><b>PHÂN PHỐI SỈ PHỤ KIỆN VÀ ĐỒ CHƠI GIÁ TỐT NHẤT</b></span></div><br>
+                    <div style="text-align: center;">
+                    <span style="color: red; font-size: 35px;"><b>PHỤ KIỆN VT<br>A34 QL22, PHƯỜNG TRUNG MỸ TÂY, Q.12, HCM</b></span></div>
+                    <div style="text-align: center;">
+                    <br></div>
+                    <div style="text-align: center;">
+                    <span style="color: blue; font-size: x-large;">GIÁ LUÔN LUÔN THẤP NHẤT - CẠNH TRANH NHẤT - BẢO HÀNH ĐỔI MỚI</span></div>
+                    <div style="text-align: center;">
+                    <span style="color: blue; font-size: x-large;">MUA 1 SẢN PHẨM CŨNG TÍNH GIÁ SỈ - MUA SỐ LƯỢNG NHIỀU CALL ĐỂ CÓ GIÁ TỐT HƠN</span></div>
+                    <div style="text-align: center;">
+                    <br></div>
 
+                    <div style="text-align: center;">
+                    <span style="font-size: x-large;background-color: yellow;color:red"><b>THÔNG TIN HÀNG HÓA VÀ GIÁ GIẢM CẬP NHẬT HÀNG NGÀY TRÊN WEBSITE NÀY<br>Đặt hàng qua điện thoại: 0166.381.0003 (Mr. Vinh) hoặc Zalo 0166.381.0003</b></span></div>
+
+                    <br>
+                    <div style="text-align: center;">
+                    <span style="font-size: large;color:blue"><b>Đặt hàng qua mail: thevinh205@gmail.com</b></span></div><br>
+                    <div style="text-align: center;">
+                    <div style="text-align: center;">
+                    <span style="font-size: x-large;color: RED"><b>THỜI GIAN LÀM VIỆC:<br>8h đến 21h hằng ngày</b></span></div>
+                    <div style="text-align: center;">
+                    </div><br>
+
+                    <br><div style="text-align: center;">
+                    <span style="font-size: large;"><b>-------*-------</b></span></div><br>
+
+                    <br>
+                    <div style="text-align: center;"><span style="font-size: large;"><b><span style="color: #A21C1C;"><b>Thông báo:</b> Khách ở nội thành HCM mua hàng vẫn được giao hàng thu tiền tận nơi bằng cách nhắn tin đặt hàng qua số điện thoại 0166.381.0003 hoặc gửi mail đặt hàng vào thevinh205@gmail.com. Khách ở Tỉnh đặt hàng với hóa đơn trên 500k và phải chuyển khoản trước đủ hết tiền hàng (cộng phí gửi nếu có) thì cửa hàng mới gửi hàng được. Kính mong quý khách hàng ở xa thông cảm!</span></b></span></div><br>
+                </div>
+                    
+                <div class="product-list">
+                    <h2 class="title">HÀNG MỚI VỀ LẠI + MẪU MỚI</h2>
+                </div>
+                    
+                <center>
+                    <table border="1" bordercolor="#BABABA" class="cms_table_grid" width="100%" align="center" style="border-color: #5bc0eb;background: #fffdcc;">
+                        <tbody>
+                            <tr>
+                                <td colspan="5" style="text-align:center;">
+                                    <b style="text-align:center;background-color: yellow;color:red;font-size: 25px;">HÀNG MỚI VỀ LẠI + MẪU MỚI (CẬP NHẬT THƯỜNG XUYÊN)<br>
+                                        mới cập nhật lúc 10H25 - 7/11/17
+                                    </b>
+                                    <br>
+                                </td>
+                            </tr>
+                            
+                            <tr valign="top">
+                                <td style="text-align: center; width: 30%">
+                                    <a href="http://anh.24h.com.vn/upload/1-2017/images/2017-03-09/1489057633-148905722366850-17.jpg" imageanchor="1" target="_blank">
+                                        <img border="0" src="http://anh.24h.com.vn/upload/1-2017/images/2017-03-09/1489057633-148905722366850-17.jpg" alt="Điện thoại Nokia 3310 full box" title="Điện thoại Nokia 3310 full box"></a>
+                                </td>
+                                <td align="center" style="width: 15%">
+                                    <span>
+                                        <font size="4">
+                                        <b>
+                                            <font color="purple">215</font>
+                                        </b> 
+                                        </font>
+                                    </span>
+                                </td>
+                                <td style="width: 40%">
+                                    <span>
+                                        <font size="4">
+                                        <b>
+                                            <a href="http://www.phukientinnhiem.com" title="215k - Điện thoại Nokia 3310 full box" target="_blank">Điện thoại <font color="red">Nokia 3310 full box</font>
+                                            </a>
+                                        </b> 
+                                        </font>
+                                        <b>
+                                            <font color="green" size="3" style="background-color: yellow"> MỚI VỀ HÀNG 10/11</font>
+                                            <font size="3" color="#7554F5" class="motasanpham"> </font>
+                                        </b>
+                                    </span>
+                                </td>
+                                <td align="center" style="width: 15%">
+                                    <span>
+                                        <font size="3"><b><font color="#000">3T</font></b></font>
+                                    </span>
+                                </td>
+                            </tr>
+                            
+                            <tr valign="top">
+                                <td style="text-align: center; width: 30%">
+                                    <a href="http://anh.24h.com.vn/upload/1-2017/images/2017-03-09/1489057633-148905722366850-17.jpg" imageanchor="1" target="_blank">
+                                        <img border="0" src="http://anh.24h.com.vn/upload/1-2017/images/2017-03-09/1489057633-148905722366850-17.jpg" alt="Điện thoại Nokia 3310 full box" title="Điện thoại Nokia 3310 full box"></a>
+                                </td>
+                                <td align="center" style="width: 15%">
+                                    <span>
+                                        <font size="4">
+                                        <b>
+                                            <font color="purple">215</font>
+                                        </b> 
+                                        </font>
+                                    </span>
+                                </td>
+                                <td style="width: 40%">
+                                    <span>
+                                        <font size="4">
+                                        <b>
+                                            <a href="http://www.phukientinnhiem.com" title="215k - Điện thoại Nokia 3310 full box" target="_blank">Điện thoại <font color="red">Nokia 3310 full box</font>
+                                            </a>
+                                        </b> 
+                                        </font>
+                                        <b>
+                                            <font color="green" size="3" style="background-color: yellow"> MỚI VỀ HÀNG 10/11</font>
+                                            <font size="3" color="#7554F5" class="motasanpham"> </font>
+                                        </b>
+                                    </span>
+                                </td>
+                                <td align="center" style="width: 15%">
+                                    <span>
+                                        <font size="3"><b><font color="#000">3T</font></b></font>
+                                    </span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </center>
+                <?php include 'template/footer.php';?> 
             </div>
         </div>
-   
+        
     </body>
 </html>
---
+++ template/footer.php
@@ -0,0 +1,72 @@
+<html>
+    <div class="footer-wrapper">
+        <div class="container">
+<footer class="clearfix">
+<div class="footer-wdigets clearfix">
+<div class="footer-section section" id="section-2"><div class="widget HTML" data-version="1" id="HTML3">
+<div class="widget-content">
+<p style="
+    font-size: 25px;
+    FONT-WEIGHT: bold;
+    color: white;
+    font-family: verdana;
+">PHỤ KIỆN TÍN NHIỆM</p>
+<div class="box-content about">
+					<p>Phân phối giá sỉ phụ kiện điện thoại, máy tính, đồ chơi, đồ gia dụng giá rẻ nhất. Chuyên pin sạc dự phòng, tai nghe bluetooth, loa bluetooth, màn hình LCD, bàn phím, chuột các loại.</p>						<ul class="list-unstyled list space-top-20">							<li><i class="fa fa-map-marker"></i>A34 QL22, Trung Mỹ Tây, quận 12, HCM</li>							<li><i class="fa fa-phone"></i>0166.381.0003 (Mr. Vinh)</li>							<li><i class="fa fa-envelope-o"></i>thevinh205@gmail.com</li>
+
+
+
+
+<li><i class="fa fa-hand-o-right"></i>Đại diện: Nguyễn Hoàng Thế Vinh - 0166.381.0003</li>
+
+
+						</ul>				</div>
+</div>
+<div class="clear"></div>
+<div class="clear"></div>
+</div></div>
+<div class="footer-section section" id="section-3"><div class="widget HTML" data-version="1" id="HTML7">
+<h2 class="title"><span>Thông tin chuyển khoản</span></h2>
+<div class="widget-content">
+<div class="box-content">
+<p>Chủ TK: <b>Nguyễn Hoàng Thế Vinh</b></p>
+<p><b style="color:#5BC0EB;">+</b> Vietcombank: 000000 - CN HCM</p>
+<p><b style="color:#5BC0EB;">+</b> Sacombank: 0601.5689.7143 - CN Cộng Hòa</p>
+
+<p>Chủ TK: <b>Nguyễn Thị Thùy Trang</b></p>
+<p><b style="color:#5BC0EB;">+</b> Agribank: 000000 - CN Suối Tân</p>
+
+<p>Quý khách vui lòng ghi rõ Nội dung chuyển khoản với Tên / SĐT</p>
+			</div>
+</div>
+<div class="clear"></div>
+<div class="clear"></div>
+</div></div>
+<div class="footer-section section" id="section-4"><div class="widget HTML" data-version="1" id="HTML10">
+<div class="widget-content">
+    <img src="http://online.gov.vn/PublicImages/2015/08/27/11/20150827110756-dathongbao.png" style="width: 350px">
+</div>
+<div class="clear"></div>
+<div class="clear"></div>
+</div><div class="widget HTML" data-version="1" id="HTML8">
+<div class="widget-content">
+
+</div>
+<div class="clear"></div>
+<div class="clear"></div>
+</div><div class="widget HTML" data-version="1" id="HTML5">
+<h2 class="title"><span>Thời gian làm việc</span></h2>
+<div class="widget-content">
+<div class="box-content">
+<p>T2 - T7  : ------------- 9h - 18h</p>
+<p>Chủ Nhật: ------------- 9h - 14h</p>				</div>
+</div>
+<div class="clear"></div>
+<div class="clear"></div>
+</div></div>
+<div class="clear"></div>
+</div>
+</footer>
+</div>
+    </div>
+</html>
