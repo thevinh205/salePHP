9a00aba91a2e54150c72542030618494f323dd57
VinhNHT <vinh.the@nxcomm.com>
2018-04-17 16:05:45 +0700

    add api check status ext

--- manager/shop/statistic.php
+++ manager/shop/statistic.php
@@ -40,27 +40,27 @@
             $sql = "";
             if($toDate != ''){
                 $toDate = date('Y-m-d',strtotime($toDate . "+1 days"));
-                $sql="SELECT total_price, status FROM order_header where shop_id= $shopId and create_date>='$fromDate' and create_date<'$toDate' and status != 'cancle'";
+                $sql="SELECT total_price, shipment_fee, status FROM order_header where shop_id= $shopId and create_date>='$fromDate' and create_date<'$toDate' and status != 'cancle'";
             } else {
-                $sql="SELECT total_price, status FROM order_header where shop_id= $shopId and create_date>='$fromDate' and create_date<NOW() and status != 'cancle'";
+                $sql="SELECT total_price, shipment_fee, status FROM order_header where shop_id= $shopId and create_date>='$fromDate' and create_date<NOW() and status != 'cancle'";
             }
             $result=mysqli_query($con,$sql);
             while($tv_3=mysqli_fetch_array($result)){
-                $total += $tv_3['total_price'];
+                $total += ($tv_3['total_price'] + $tv_3['shipment_fee']);
                 if($tv_3['status'] == 'resolve')
-                    $danhan += $tv_3['total_price'];
+                    $danhan += ($tv_3['total_price'] + $tv_3['shipment_fee']);
             }
             $danggiao = $total - $danhan;
         //lợi nhuận
-            $sql = "";
-            if($toDate != ''){
-                $sql="SELECT sum(price_buy*count) as cost FROM order_header od LEFT JOIN order_party_relationship op ON od.id=op.order_id LEFT JOIN product p on op.product_id=p.id where od.shop_id= $shopId and op.create_date>='$fromDate' and op.create_date<'$toDate' and od.status = 'resolve'";
-            } else {
-                $sql="SELECT sum(price_buy*count) as cost FROM order_header od LEFT JOIN order_party_relationship op ON od.id=op.order_id LEFT JOIN product p on op.product_id=p.id where od.shop_id= $shopId and op.create_date>='$fromDate' and op.create_date<NOW() and od.status = 'resolve'";
-            }
-            $result=mysqli_query($con,$sql);
-            $data = mysqli_fetch_assoc($result);
-            $profit = $danhan - $data['cost'];
+//            $sql = "";
+//            if($toDate != ''){
+//                $sql="SELECT sum(price_buy*count) as cost FROM order_header od LEFT JOIN order_party_relationship op ON od.id=op.order_id LEFT JOIN product p on op.product_id=p.id where od.shop_id= $shopId and op.create_date>='$fromDate' and op.create_date<'$toDate' and od.status = 'resolve'";
+//            } else {
+//                $sql="SELECT sum(price_buy*count) as cost FROM order_header od LEFT JOIN order_party_relationship op ON od.id=op.order_id LEFT JOIN product p on op.product_id=p.id where od.shop_id= $shopId and op.create_date>='$fromDate' and op.create_date<NOW() and od.status = 'resolve'";
+//            }
+//            $result=mysqli_query($con,$sql);
+//            $data = mysqli_fetch_assoc($result);
+//            $profit = $danhan - $data['cost'];
             
         //chi phí
             $sql = "SELECT sum(total) as cost FROM spend sp where sp.shop_id=$shopId and sp.create_date>='$fromDate' ";
@@ -84,7 +84,7 @@
                 <th style="text-align: center">Tiền đã nhận</th>
                 <th style="text-align: center">Hàng đang giao</th>
                 <th style="text-align: center">Chi tiêu</th>
-                <th style="text-align: center">Tổng lợi nhuận</th>
+<!--                <th style="text-align: center">Tổng lợi nhuận</th>-->
                 <th style="text-align: center">Tổng tiền mặt</th>
             </tr>
           </thead>
@@ -94,7 +94,7 @@
                   <td style='font-weight: bold; font-size: 18px; text-align: center'><?php echo "<span class='price-total numbers'>$danhan</span> VNĐ"; ?></td>
                   <td style='font-weight: bold; font-size: 18px; text-align: center'><?php echo "<span class='price-total numbers'>$danggiao</span> VNĐ"; ?></td>
                   <td style='font-weight: bold; font-size: 18px; text-align: center'><?php echo "<span class='price-total numbers'>$spend</span> VNĐ"; ?></td>
-                  <td style='font-weight: bold; font-size: 18px; text-align: center'><?php echo "<span class='price-total numbers'>$profit</span> VNĐ"; ?></td>
+<!--                  <td style='font-weight: bold; font-size: 18px; text-align: center'><?php echo "<span class='price-total numbers'>$profit</span> VNĐ"; ?></td>-->
                   <td style='font-weight: bold; font-size: 18px; text-align: center'><?php echo "<span class='price-total numbers'>$totalCast</span> VNĐ"; ?></td>
               </tr>
           </tbody>
